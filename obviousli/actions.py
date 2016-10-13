0f76b4023d9c8e911bc578eca9bccb14824db966
Arun Tejasvi Chaganty <arunchaganty@gmail.com>
2016-10-13 14:32:37 -0700

    Added a lexical paraphrase action and template

--- obviousli/actions.py
+++ obviousli/actions.py
@@ -4,6 +4,7 @@
 Definitions of standard actions
 """
 
+from abc import abstractmethod
 from .defs import State, Action, Truth
 
 class GiveUpAction(Action):
@@ -19,9 +20,66 @@ class GiveUpAction(Action):
         state.truth = Truth.NEUTRAL
         return state
 
+    def __str__(self):
+        return "[GiveUp()]"
+
+class ActionTemplate(object):
+    """
+    Can be applied on a state to generate actions.
+    """
+
+    @abstractmethod
+    def generate(self, state):
+        """
+        Generate all possible applications of this template to the state.
+        """
+        return ()
+
 # TODO(chaganty): implement the following actions.
 #     - Guess
 #     - LexicalParaphrase
+class LexicalParaphrase(Action):
+    """
+    Replace a lexical sequence.
+    """
+    def __init__(self, input_, output, apply_on_source=True, match_idx=None):
+        super(LexicalParaphrase, self).__init__(None)
+        self.input = input_
+        self.output = output
+        self.apply_on_source = apply_on_source
+        self.match_idx = match_idx
 
-#     - SyntacticParaphrase
-#     - SyntacticTransformation
+    def __str__(self):
+        return "[LexicalParaphrase({}:{}@{} -> {})]".format("S" if self.apply_on_source else "T", self.input, self.match_idx, self.output)
+
+    def __call__(self, state):
+        if self.apply_on_source:
+            state.source = state.source[:self.match_idx] + self.output + state.source[self.match_idx + len(self.input):]
+        else:
+            state.target = state.target[:self.match_idx] + self.output + state.target[self.match_idx + len(self.input):]
+        return state
+
+class LexicalParaphraseTemplate(ActionTemplate):
+    """
+    Generates LexicalParaphrase actions for a given 'input'/'output' pair.
+    """
+
+    def __init__(self, input_, output):
+        self.input = input_
+        self.output = output
+
+    def generate(self, state):
+        # check if input is in the source.
+        start = 0
+        while state.source.find(self.input, start) > -1:
+            idx = state.source.find(self.input, start)
+            yield LexicalParaphrase(self.input, self.output, apply_on_source=True, match_idx=idx)
+            start = idx+1
+
+        start = 0
+        while state.target.find(self.input, start) > -1:
+            idx = state.target.find(self.input, start)
+            yield LexicalParaphrase(self.input, self.output, apply_on_source=False, match_idx=idx)
+            start = idx+1
+
+# - PhraseParaphrase
--- tests/test_actions.py
+++ tests/test_actions.py
@@ -2,13 +2,34 @@
 # -*- coding: utf-8 -*-
 
 from obviousli.defs import State, Truth
-from obviousli.actions import GiveUpAction
+from obviousli.actions import GiveUpAction, LexicalParaphrase, LexicalParaphraseTemplate
 
 def test_give_up_action():
-    state = State("E=MC2", "Matter and energy are interchangable", Truth.TRUE, None)
+    state = State("E=MC^2", "Matter and energy are interchangable", Truth.TRUE, None)
     action = GiveUpAction()
     state_ = action(state)
 
     assert state_.source == "Matter and energy are interchangable"
     assert state_.isEnd()
+    assert state.truth == Truth.NEUTRAL
 
+def test_lexical_paraphrase():
+    state = State("Obama shouted at the speaker.", "Obama screamed at the speaker.", Truth.TRUE, None)
+    action = LexicalParaphrase("shouted", "screamed", True, 6)
+    state_ = action(state)
+
+    assert state_.source == "Obama screamed at the speaker."
+    assert state_.isEnd()
+    assert state.truth == Truth.TRUE
+
+def test_lexical_paraphrase_template():
+    state = State("Obama shouted at the speaker.", "Obama screamed at the speaker.", Truth.TRUE, None)
+    template = LexicalParaphraseTemplate("shouted", "screamed")
+    actions = list(template.generate(state))
+
+    assert len(actions) == 1
+    action = actions[0]
+    assert action.input == "shouted"
+    assert action.output == "screamed"
+    assert action.apply_on_source
+    assert action.match_idx == 6
